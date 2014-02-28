15c94e493d5e9a417356012c5fc3475dc552d4cf
Tomonori Konno <sktomorg@gmail.com>
2014-02-28 23:32:18 +0900

    refactor agent.h

--- agent.h
+++ agent.h
@@ -40,37 +40,38 @@ init_agent(Agent * agent)
 }
 
 void
-init_agents(Agent * agents, int num_agents)
+init_agents(Agent * agents, int number_agents)
 {
   Agent * agent = agents;
-  for(; num_agents--; ++agent)
+  for(; number_agents--; ++agent)
   {
     init_agent(agent);
     agent->refresh(agent);
   }
 }
 
+double * asks = NULL;
+double * bids = NULL;
 void
-minmax(double * min_ask, double * max_bid, Agent * agents, int num_agents)
+minmax(double * min_ask, double * max_bid, Agent * agents, int number_agents)
 {
-  double * asks;
-  double * bids;
-  bids = malloc(sizeof(double) * num_agents);
-  asks = malloc(sizeof(double) * num_agents);
+  if(!asks)
+  {
+    bids = malloc(sizeof(double) * number_agents);
+    asks = malloc(sizeof(double) * number_agents);
+  }
 
   double * ask;
   double * bid;
   Agent * agent = agents;
   int i;
-  for(ask = asks, bid = bids, i = num_agents; i--; ++agent, ++ask, ++bid)
+  for(ask = asks, bid = bids, i = number_agents; i--; ++agent, ++ask, ++bid)
   {
     *ask = agent->ask;
     *bid = agent->bid;
   }
 
-  *min_ask = min(asks, num_agents);
-  *max_bid = max(bids, num_agents);
-  free(asks);
-  free(bids);
+  *min_ask = min(asks, number_agents);
+  *max_bid = max(bids, number_agents);
 }
 
